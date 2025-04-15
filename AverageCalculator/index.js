a976e803ba581f37ff921d57f54e02270de6d321
aryan-chopra <aryan.chopra1211@gmail.com>
2025-04-15 12:32:05 +0530

    Testing receiving numberId

--- AverageCalculator/index.js
+++ AverageCalculator/index.js
@@ -3,7 +3,15 @@ import express from "express"
 const App = express()
 
 App.get('/numbers/:numberId', (req, res) => {
-    res.send(req.params.numberId)
+    if (req.params.numberId === 'p') {
+        res.send("Prime")
+    } else if (req.params.numberId === 'f') {
+        res.send("Fibonacci")
+    } else if (req.params.numberId === 'e') {
+        res.send("Even")
+    } else if (req.params.numberId === 'r') {
+        res.send("Random")
+    }
 })
 
 App.listen("8080")
