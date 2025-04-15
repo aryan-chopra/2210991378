76a1b3dc3d4da3186605615e6e5ff616254a36f8
aryan-chopra <aryan.chopra1211@gmail.com>
2025-04-15 12:19:09 +0530

    Testing

--- AverageCalculator/index.js
+++ AverageCalculator/index.js
@@ -1,1 +1,10 @@
+import express from "express"
 
+const App = express()
+
+App.get('/numbers/numberId', (req, res) => {
+    res.send(req.params.numberId)
+})
+
+App.listen("8080")
+console.log("Listening")
