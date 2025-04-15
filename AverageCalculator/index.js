822ba29be49189dfb39001b915eb496eace05564
aryan-chopra <aryan.chopra1211@gmail.com>
2025-04-15 12:52:26 +0530

    Fixed no response

--- AverageCalculator/index.js
+++ AverageCalculator/index.js
@@ -11,16 +11,41 @@ App.get('/numbers/:numberId', async (req, res) => {
     // Get numbers from somewhere
 
     if (req.params.numberId === 'p') {
-        response = await fetch("http://20.244.56.144/evaluation-service/primes")
+        response = await fetch("http://20.244.56.144/evaluation-service/primes", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0NzAxNjg5LCJpYXQiOjE3NDQ3MDEzODksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImUyYWU0OWNjLWNlYTYtNDRlNy1iZjM2LTFhZTkyOGQyYWNkMCIsInN1YiI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiJ9LCJlbWFpbCI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiIsIm5hbWUiOiJhcnlhbiBjaG9wcmEiLCJyb2xsTm8iOiIyMjEwOTkxMzc4IiwiYWNjZXNzQ29kZSI6IlB3enVmRyIsImNsaWVudElEIjoiZTJhZTQ5Y2MtY2VhNi00NGU3LWJmMzYtMWFlOTI4ZDJhY2QwIiwiY2xpZW50U2VjcmV0IjoiTUFKbVh2WHFFTVNoQ2RIRSJ9.oz4_ENEc2uHtA3wIwpPHAQhEWlToLj61rizBlMb3l_s'
+            }
+        })
     } else if (req.params.numberId === 'f') {
-        response = await fetch("http://20.244.56.144/evaluation-service/fibo")
+        response = await fetch("http://20.244.56.144/evaluation-service/primes", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0NzAxNjg5LCJpYXQiOjE3NDQ3MDEzODksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImUyYWU0OWNjLWNlYTYtNDRlNy1iZjM2LTFhZTkyOGQyYWNkMCIsInN1YiI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiJ9LCJlbWFpbCI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiIsIm5hbWUiOiJhcnlhbiBjaG9wcmEiLCJyb2xsTm8iOiIyMjEwOTkxMzc4IiwiYWNjZXNzQ29kZSI6IlB3enVmRyIsImNsaWVudElEIjoiZTJhZTQ5Y2MtY2VhNi00NGU3LWJmMzYtMWFlOTI4ZDJhY2QwIiwiY2xpZW50U2VjcmV0IjoiTUFKbVh2WHFFTVNoQ2RIRSJ9.oz4_ENEc2uHtA3wIwpPHAQhEWlToLj61rizBlMb3l_s'
+            }
+        })
     } else if (req.params.numberId === 'e') {
-        response = await fetch("http://20.244.56.144/evaluation-service/even")
+        response = await fetch("http://20.244.56.144/evaluation-service/primes", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0NzAxNjg5LCJpYXQiOjE3NDQ3MDEzODksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImUyYWU0OWNjLWNlYTYtNDRlNy1iZjM2LTFhZTkyOGQyYWNkMCIsInN1YiI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiJ9LCJlbWFpbCI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiIsIm5hbWUiOiJhcnlhbiBjaG9wcmEiLCJyb2xsTm8iOiIyMjEwOTkxMzc4IiwiYWNjZXNzQ29kZSI6IlB3enVmRyIsImNsaWVudElEIjoiZTJhZTQ5Y2MtY2VhNi00NGU3LWJmMzYtMWFlOTI4ZDJhY2QwIiwiY2xpZW50U2VjcmV0IjoiTUFKbVh2WHFFTVNoQ2RIRSJ9.oz4_ENEc2uHtA3wIwpPHAQhEWlToLj61rizBlMb3l_s'
+            }
+        })
     } else if (req.params.numberId === 'r') {
-        response = await fetch("http://20.244.56.144/evaluation-service/rand")
+        response = await fetch("http://20.244.56.144/evaluation-service/primes", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0NzAxNjg5LCJpYXQiOjE3NDQ3MDEzODksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImUyYWU0OWNjLWNlYTYtNDRlNy1iZjM2LTFhZTkyOGQyYWNkMCIsInN1YiI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiJ9LCJlbWFpbCI6ImFyeWFuMTM3OC5iZTIyQGNoaXRrYXJhLmVkdS5pbiIsIm5hbWUiOiJhcnlhbiBjaG9wcmEiLCJyb2xsTm8iOiIyMjEwOTkxMzc4IiwiYWNjZXNzQ29kZSI6IlB3enVmRyIsImNsaWVudElEIjoiZTJhZTQ5Y2MtY2VhNi00NGU3LWJmMzYtMWFlOTI4ZDJhY2QwIiwiY2xpZW50U2VjcmV0IjoiTUFKbVh2WHFFTVNoQ2RIRSJ9.oz4_ENEc2uHtA3wIwpPHAQhEWlToLj61rizBlMb3l_s'
+            }
+        })
     }
 
-    res.send(response)
+    let jsonRes = await response.json()
+    res.send(jsonRes)
 })
 
 App.listen("8080")
